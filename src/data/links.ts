76c1b49bcc09c82cc823056f64bc341b70333626
Wyxx775 <zuuroff@gmail.com>
2026-07-03 16:32:07 +0200

    Delete src directory

--- src/data/links.ts
+++
@@ -1,8 +0,0 @@
-// Remplacez ces liens par les vôtres.
-export const DISCORD_INVITE_URL = 'https://discord.gg/votre-invitation'
-
-export const TAX_DECLARATION_FORM_URL =
-  'https://forms.gle/votre-formulaire-declaration-impots'
-
-export const BUSINESS_CREATION_FORM_URL =
-  'https://forms.gle/votre-formulaire-creation-entreprise'
